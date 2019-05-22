d029a5de1f0510ec214c740f009ebab0807cf1eb
thepeshka <peshka5@yandex.ru>
2019-05-22 13:04:40 +0300

    Create init.py

---
+++ scripts/init.py
@@ -0,0 +1,15 @@
+import configparser
+import sys
+
+config = configparser.ConfigParser()
+config['MAIN']['domain'] = sys.argv[1]
+i = 2
+while sys.argv[i]:
+    config['USERS'][sys.argv[i]] = sys.argv[i+1]
+    i += 2
+with open('settings.ini', 'w+') as configfile:
+    config.write(configfile)
+    
+users = ",".join([f"{username}:{config['USERS'][username]}" for username in config['USERS']])
+with open('.env', 'w') as envfile:
+    envfile.writelines([f'maildomain={domain}', f'smtp_user={users}'])
